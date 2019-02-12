507b17f4fafe48ef63e4a49f924b44ee35a5bef2
Matthew Andrews <matthew.andrews@mouser.com>
2019-02-12 10:41:12 -0600

    Add Raude-specific changes

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello Mouser!"
 
+@app.route("/raude")
+def raude():
+    return "Hi boss!"
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
